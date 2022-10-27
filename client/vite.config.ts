b670b0d9900f7a8cc29a62958945eb6dc7d2cc9c
Michał Lipiński <112834462+JusttCallMeMike@users.noreply.github.com>
2022-10-27 09:49:37 +0200

    run in paralell

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import * as dotenv from 'dotenv';
 import react from '@vitejs/plugin-react'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      // with options
+      '/api': {
+        target: process.env.ORIGIN,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    }
+  }
 })
