cf01dd1e77aa36bc2a4afe3777e6e4d4a2991b02
Michał Lipiński <112834462+JusttCallMeMike@users.noreply.github.com>
2022-10-30 00:27:21 +0200

    layout

--- server/src/services/kline.service.ts
+++ server/src/services/kline.service.ts
@@ -1,28 +1,28 @@
 import dayjs from "dayjs";
 import { prisma } from "../utils/prisma";
-import {match,P} from 'ts-pattern'
+import { match, P } from "ts-pattern";
 import { KlineSelectorType } from "../schema/kline.schema";
 
 export const getKlines = async ({
-    pair,
-    period,
-    dateFrom,
-    dateTo,
-    limit
-  }: KlineSelectorType) => {
-
-
+  pair,
+  period,
+  dateFrom,
+  dateTo,
+  limit,
+}: KlineSelectorType) => {
   if (!dateTo && !limit) throw new Error("dateTo or limit must be provided");
-  if (dateTo && limit) throw new Error("dateTo or limit must be provided, not both");
+  if (dateTo && limit)
+    throw new Error("dateTo or limit must be provided, not both");
 
   const gte = dayjs(dateFrom).toISOString();
-  let lte =""
-  if(dateTo)
-      lte=dayjs(dateTo).toISOString()
-  if(limit) {
-      const unit = period.slice(-1) as "m" | "h" | "d";
-      const interval = Number(period.slice(0, -1));
-      lte=dayjs(dateFrom).add(limit*interval, unit).toISOString()
+  let lte = "";
+  if (dateTo) lte = dayjs(dateTo).toISOString();
+  if (limit) {
+    const unit = period.slice(-1) as "m" | "h" | "d";
+    const interval = Number(period.slice(0, -1));
+    lte = dayjs(dateFrom)
+      .add(limit * interval, unit)
+      .toISOString();
   }
   const results = await prisma.kline.findMany({
     where: {
@@ -33,17 +33,17 @@ export const getKlines = async ({
         lte,
       },
     },
+    orderBy: [{ timestamp: "asc" }],
   });
 
-//   let ltez = match([dateTo, limit] as const)
-//   .with([P.union(P.instanceOf(Dayjs), P.instanceOf(Date), P.string ), undefined], ()=>dayjs(dateTo).toISOString())
-//   .with([undefined, P.number], ()=>{
-//           const unit = period.slice(-1) as "m" | "h" | "d";
-//           const interval = Number(period.slice(0, -1));
-//           return dayjs(dateFrom).add(limit*interval, unit).toISOString()
-//       })
-//       .with([P._,P.number],()=>limit*1)
-//       .exhaustive()
-  return results
+  // let ltez = match([dateTo, limit] as const)
+  // .with([P.union(P.instanceOf(Dayjs), P.instanceOf(Date), P.string ), undefined], ()=>dayjs(dateTo).toISOString())
+  // .with([undefined, P.number], ()=>{
+  //         const unit = period.slice(-1) as "m" | "h" | "d";
+  //         const interval = Number(period.slice(0, -1));
+  //         return dayjs(dateFrom).add(limit*interval, unit).toISOString()
+  //     })
+  //     .with([P._,P.number],()=>limit*1)
+  //     .exhaustive()
+  return results;
 };
-
